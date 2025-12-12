b45e880587f513a7c92d84e5821419f0b172b480
TATCHIwillyjunior <tatchi-feudjio.willy-junior@epita.fr>
2025-12-12 13:46:28 +0100

    First Commit

---
+++ src/main/java/fr/epita/timeoutAirlineBookingSystem/TimeoutAirlineBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package fr.epita.timeoutAirlineBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeoutAirlineBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeoutAirlineBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epita/timeoutAirlineBookingSystem/TimeoutAirlineBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epita.timeoutAirlineBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeoutAirlineBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
